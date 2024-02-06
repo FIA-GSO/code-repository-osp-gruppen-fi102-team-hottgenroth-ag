299628d8c75491e3edb6c2718d3dc13cbf002d81
Rene Müller <mueller.rene99@gmail.com>
2024-02-06 13:03:34 +0100

    refactored login page

--- logistics-app/src/app/features/login/login.component.ts
+++ logistics-app/src/app/features/login/login.component.ts
@@ -4,10 +4,12 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule,MatButtonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatCardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
