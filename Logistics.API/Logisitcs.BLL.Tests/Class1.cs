2208329aec8c956436a3ba5d80cb5772ae4b3c45
Artur Sartison <>
2024-03-05 20:59:13 +0100

    3700 - Test Projekte gehen jetzt

--- Logistics.API/Logisitcs.BLL.Tests/Class1.cs
+++ Logistics.API/Logisitcs.BLL.Tests/Class1.cs
@@ -1,63 +1,16 @@
-﻿using Xunit;
-using Moq;
-using Logisitcs.BLL.Interfaces;
-using Logisitcs.BLL.Models;
-using Logisitcs.DAL;
-using Logisitcs.DAL.Models;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using NUnit.Framework;
 
 namespace Logisitcs.BLL.Tests
 {
-    public class LoginBllTests
+    [TestFixture]
+    public class TestClass
     {
-        [Fact]
-        public async Task Login_ValidCredentials_ReturnsUserData()
+        [Test]
+        public void Test()
         {
-            // Arrange
-            var loginData = new Mock<ILoginData>();
-            loginData.SetupGet(x => x.UserEmail).Returns("test@example.com");
-            loginData.SetupGet(x => x.Password).Returns("password");
-
-            var mockDBCommands = new Mock<IDBCommands>();
-            mockDBCommands.Setup(x => x.GetUserByMail(It.IsAny<string>())).Returns(new User
-            {
-                UserId = "123",
-                UserEmail = "test@example.com",
-                UserPassword = PasswordHashHelper.Hash("password"),
-                UserRoleId = 1
-            });
-
-            var loginBll = new LoginBll(mockDBCommands.Object);
-
-            // Act
-            var result = await loginBll.Login(loginData.Object);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equal("123", result.UserId.ToString());
-            Assert.Equal("test@example.com", result.UserEmail);
-            Assert.Equal("Admin", result.Role);
-        }
-
-        [Fact]
-        public async Task Register_NewUser_SuccessfullyRegistered()
-        {
-            // Arrange
-            var loginData = new Mock<ILoginData>();
-            loginData.SetupGet(x => x.UserEmail).Returns("newuser@example.com");
-            loginData.SetupGet(x => x.Password).Returns("password");
-
-            var mockDBCommands = new Mock<IDBCommands>();
-            mockDBCommands.Setup(x => x.GetUserByMail(It.IsAny<string>())).Returns((User)null);
-
-            var loginBll = new LoginBll(mockDBCommands.Object);
-
-            // Act
-            var result = await loginBll.Register(loginData.Object);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equal("newuser@example.com", result.UserEmail);
+            int abc = 12;
+            abc.Should().Be(12);
         }
     }
 }
